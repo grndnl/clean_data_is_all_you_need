773b28b11510fdae85aa673b99200e0db4faf5d4
Carlos D. Crespo <carlos.d.crespo@berkeley.edu>
2023-12-08 20:26:07 -0800

    fixed builder script

--- app/script_build_all_containers.sh
+++ app/script_build_all_containers.sh
@@ -16,11 +16,13 @@ printf "::BUILDING $DLA_IMAGE_NAME::\n\n"
 docker build -t $DLA_IMAGE_NAME $DLA_DIRECTORY
 
 #--------------------------------------------------------------------------------------------------
-TEXT_IMAGE_NAME="clean_text_extraction:latest"
-TEXT_DIRECTORY=$CURRENT_DIR/text_extraction
+# Commented since we are not using this image
 
-printf $DIVIDER_LINE
-printf "::BUILDING $TEXT_IMAGE_NAME::\n\n"
+# TEXT_IMAGE_NAME="clean_text_extraction:latest"
+# TEXT_DIRECTORY=$CURRENT_DIR/text_extraction
 
-docker build -t $TEXT_IMAGE_NAME $TEXT_DIRECTORY
+# printf $DIVIDER_LINE
+# printf "::BUILDING $TEXT_IMAGE_NAME::\n\n"
+
+# docker build -t $TEXT_IMAGE_NAME $TEXT_DIRECTORY
 
