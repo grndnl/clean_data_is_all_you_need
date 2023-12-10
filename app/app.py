c2fe4cd3bc480fe38fba3dc666d8741ba8e25879
Daniele Grandi <daniele.grandi@autodesk.com>
2023-12-10 11:52:35 -0800

    streamlit local demo working with Carlos's code

--- app/app.py
+++ app/app.py
@@ -9,18 +9,6 @@ import json
 import requests
 
 
-def convert_docker_path_to_host(docker_path):
-    container_path, host_path = ('/app/src', 'app')
-    Path(host_path).mkdir(exist_ok=True)
-
-    # Ensure the docker_path starts with the container_path
-    if docker_path.startswith(container_path):
-        # Replace the container_path with the host_path
-        return docker_path.replace(container_path, host_path, 1)
-    else:
-        raise ValueError("Docker path does not match the volume mapping")
-
-
 def disable():
     st.session_state.disabled = True
 
@@ -56,18 +44,18 @@ def display_mask(upload_files):
             name = uploaded_file.name
 
             # load image file
-            image_file = Image.open("app/data/1603.09631_page_0001_dev_img_1_base_dla_result.jpg")
+            image_file = Image.open(f"app/data/s3_outputs/model_outputs/{name[:-4]}_page_0001_dev_img_1_base_dla_result.jpg")
 
             st.markdown(f"**{name}**")
             st.image(image_file, width=200)  # , caption='Predicted masks for the first page'use_column_width=True)
 
 
-def display_json(upload_files, json_path):
+def display_json(upload_files):
     with st.expander("**JSON Output**"):
         for uploaded_file in upload_files:
             name = uploaded_file.name
             # load json file
-            with open(json_path, "r") as f:
+            with open(f"app/data/s4_json_text_output/{name[:-4]}/{name[:-4]}.json", "r") as f:
                 json_file = json.load(f)
 
             st.divider()
@@ -75,12 +63,12 @@ def display_json(upload_files, json_path):
             st.json(json_file, expanded=False)
 
 
-def display_markdown(upload_files, text_path):
+def display_markdown(upload_files):
     with st.expander("**Txt Output**"):
         for uploaded_file in upload_files:
             name = uploaded_file.name
             # load md file
-            with open(text_path, "r", encoding="utf8") as f:
+            with open(f"app/data/s4_json_text_output/{name[:-4]}/{name[:-4]}.txt", "r", encoding="utf8") as f:
                 txt_file = f.read()
 
             st.divider()
@@ -89,51 +77,46 @@ def display_markdown(upload_files, text_path):
 
 
 def display_download_button():
-    with open('tmp/processed_files.zip', 'rb') as f:
+    with open('app/data/s4_json_text_output.zip', 'rb') as f:
         download_btn = st.download_button(label=f'processed_files.zip', file_name=f'processed_files.zip', data=f)
 
 
-def call_process_pdfs_api(uploaded_files):
-    # Prepare the files for sending
-    files = {"files": (file.name, file, "application/pdf") for file in uploaded_files}
-
+def call_process_pdfs_api():
     # API endpoint
-    api_url = "http://localhost:8000/process-pdfs/"
+    api_url = "http://localhost:8100/dla?use_cpu=false&model_type=DIT"
 
     # Make the POST request to the FastAPI backend, and error out with timeout
     try:
-        response = requests.post(api_url, files=files, timeout=3)
-    except:
-        return None, None
+        response = requests.get(api_url, timeout=120)
+    except Exception as e:
+        st.error(e)
+        return None, None, None
 
     # Process the response
     if response.status_code == 200:
-        # The API returns a list of file paths
-        response_data = response.json()
-
-        # Assuming the first element is the path to the JSON file
-        # and the second element is the path to the text file
-        # Process the first file path
-        path_parts_json = response_data[0].split(os.sep)
-        json_file_path = os.sep.join(path_parts_json)
-
-        # Process the second file path
-        path_parts_text = response_data[1].split(os.sep)
-        text_file_path = os.sep.join(path_parts_text)
-
-        return json_file_path, text_file_path
+        # From response body get exit_msg
+        exit_msg = response.json()['exit_msg']
+        output_dir = response.json()['output_directory']
+        json_txt_dir = response.json()['text_output_directory']
+        return exit_msg, output_dir, json_txt_dir
+    elif response.status_code == 500:
+        st.error(response)
     else:
         # Handle errors or unsuccessful responses
-        return None, None
+        return None, None, None
+
+
+def save_files(file_bytes):
+    # create folder if it does not exist
+    os.makedirs('app/data/s1_input_pdfs', exist_ok=True)
+
+    for file_byte in file_bytes:
+        with open(f"app/data/s1_input_pdfs/{file_byte.name}", "wb") as f:
+            f.write(file_byte.getbuffer())
 
 
 # ------------Page configs-----------------------------------------------------------------------
 st.set_page_config(layout='wide')
-
-# Clean local files  # TODO
-# clean tmp folder and delete zip file if it exists
-# shutil.rmtree('tmp', ignore_errors=True)
-# os.remove('processed_files.zip') if os.path.exists('processed_files.zip') else None
 
 
 # ------------Session State-----------------------------------------------------------------------
@@ -142,10 +125,8 @@ if 'processed_files' not in st.session_state:
     st.session_state['processed_files'] = False
 if "disabled" not in st.session_state:
     st.session_state.disabled = False
-if "json_path" not in st.session_state:
-    st.session_state.json_path = None
-if "text_path" not in st.session_state:
-    st.session_state.text_path = None
+if "output_data" not in st.session_state:
+    st.session_state.output_data = None, None, None
 
 # ----------UI logic-------------------------------------------------------------------------
 # Title of the page
@@ -169,6 +150,7 @@ with tab1:
     col1, col2, col3, col4 = st.columns([1, 0.3, 1, 1], gap='large')
     with col1:
         uploaded_files = st.file_uploader("**Upload PDFs**", accept_multiple_files=True, type='pdf', on_change=enable)
+        save_files(uploaded_files)
     with col2:
         st.write(" ")
         st.write(" ")
@@ -181,7 +163,7 @@ with tab1:
             st.write(" ")
             st.write(" ")
             # st.write(" ")
-            st.success('Processing complete!')
+            st.success(st.session_state.output_data[0])
 
     if uploaded_files:
         with col1:
@@ -200,17 +182,14 @@ with tab1:
     if uploaded_files and process_button:
         with col3:
             with st.spinner('Processing PDFs...'):
-                json_path, text_path = call_process_pdfs_api(uploaded_files)
-            if json_path is None or text_path is None:
+                exit_msg, output_dir, json_txt_dir = call_process_pdfs_api()
+            if None in [exit_msg, output_dir, json_txt_dir]:
                 st.error("Something went wrong. Please try again.")
                 st.stop()
             else:
-                st.success('Processing complete!')
-                json_path = convert_docker_path_to_host(json_path)
-                text_path = convert_docker_path_to_host(text_path)
+                st.success(exit_msg)
                 st.session_state.processed_files = True
-                st.session_state.json_path = json_path
-                st.session_state.text_path = text_path
+                st.session_state.output_data = exit_msg, output_dir, json_txt_dir
 
             time.sleep(1)
             display_mask(uploaded_files)
@@ -219,15 +198,16 @@ with tab1:
         with col4:
             # zip processed files
             with st.spinner("Zipping processed files..."):
-                shutil.make_archive('tmp/processed_files', 'zip', 'app/text_extraction/output_json_text/')
+                shutil.make_archive('app/data/s4_json_text_output', 'zip', 'app/data/s4_json_text_output')
 
+            st.write(st.session_state.processed_files)
             st.write(" ")
             st.write(" ")
             display_download_button()
             time.sleep(1)
-            display_json(uploaded_files, json_path)
+            display_json(uploaded_files)
             time.sleep(1)
-            display_markdown(uploaded_files, text_path)
+            display_markdown(uploaded_files)
 
     elif st.session_state.processed_files:  # Saved state necessary to resume once the download button is pressed
         with col3:
@@ -236,8 +216,8 @@ with tab1:
             st.write(" ")
             st.write(" ")
             display_download_button()
-            display_json(uploaded_files, st.session_state.json_path)
-            display_markdown(uploaded_files, st.session_state.text_path)
+            display_json(uploaded_files)
+            display_markdown(uploaded_files)
 
 # ----------Documentation-----------------------------------------------------------------------------
 with tab2:
