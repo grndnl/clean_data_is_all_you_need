f20d3ae62cf71901cbc2fdb52e600d7defa75d36
Daniele Grandi <grndnl@gmail.com>
2023-12-18 09:44:58 -0800

    Update app.py

--- app/app.py
+++ app/app.py
@@ -94,6 +94,7 @@ if "disabled" not in st.session_state:
 # ----------UI logic-------------------------------------------------------------------------
 # Title of the page
 st.title('📄 Clean Data is All You Need')
+st.set_page_config(page_title="📄 Clean Data is All You Need")
 
 # Sidebar for navigation and control
 with st.sidebar:
@@ -108,7 +109,7 @@ tab1, tab2 = st.tabs(["Demo", "Documentation"])
 with tab1:
     # Columns
     # 4 columns of different sizes
-    col1, col2, col3, col4 = st.columns([1, 0.3, 1, 1], gap='large')
+    col1, col2, col3, col4 = st.columns([1, 1, 1, 1], gap='large')
     with col1:
         # dropdown that allows to select one PDF file
         uploaded_files = st.selectbox("Select a PDF file", ["1603.01514.pdf", "1604.00727.pdf"])
